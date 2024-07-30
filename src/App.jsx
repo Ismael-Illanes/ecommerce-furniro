349055aa8737575ef17e03c6c51b67a6db2bfe74
Ismael Illanes <iiv94@hotmail.com>
2024-07-30 13:05:53 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './styles/app.css'
+import Header from './components/Header'
+
+
+
+function App() {
+/*   const [count, setCount] = useState(0) */
+
+  return (
+    <div id='root'>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
