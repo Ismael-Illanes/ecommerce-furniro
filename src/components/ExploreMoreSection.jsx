54e110b1469d8520c6babe0c10079ad35e16da60
Ismael Illanes <iiv94@hotmail.com>
2025-03-17 11:39:55 +0100

    Few additions to component ExploreMoreSection

--- src/components/ExploreMoreSection.jsx
+++ src/components/ExploreMoreSection.jsx
@@ -1,3 +1,4 @@
+import carouselImages from "../helpers/carouselImages";
 import "../styles/exploreSection.scss";
 
 export default function ExploreMoreSection() {
@@ -5,11 +6,25 @@ export default function ExploreMoreSection() {
     <div className="explore-section">
       <div className="explore-section__content">
         <div className="explore-section__content__text">
-            <div className="explore-section__content__text__dialog">
-                <h1>50+ Beautiful rooms inspiration</h1>
-            </div>
+          <div className="explore-section__content__text__title">
+            <h2>50+ Beautiful rooms inspiration</h2>
+          </div>
+          <div className="explore-section__content__text__description">
+            <p>Our designer already made a lot of beautiful prototipe of rooms that inspire you</p>
+          </div>
+          <button className="explore-section__content__text__btn">Explore More</button>
         </div>
-        <div className="explore-section__content__images"></div>
+        <div className="explore-section__content__images">
+          <picture>
+            <source srcSet={carouselImages.carousel1} width={"375px"} height={"500px"} />
+            <img
+              src={carouselImages.carousel1}
+              width="100%"
+              height="100%"
+              alt="Banner"
+            />
+          </picture>
+        </div>
       </div>
     </div>
   )
---
+++ src/helpers/carouselImages.js
@@ -0,0 +1,10 @@
+import carousel1 from '../assets/carousel1.webp';
+
+
+const carouselImages = {
+  carousel1,
+};
+
+
+
+export default carouselImages;
